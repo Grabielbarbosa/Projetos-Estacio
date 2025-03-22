66fe7620c788e3fca7f85924d790ec0d777f1ad9
Grabielbarbosa <gbsilva98@gmail.com>
2025-03-22 01:38:41 +0000

    atualizações

---
+++ OPERAÇÕES_MATEMATICAS/operadores_relacionais.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+//  a > b (maior que)
+//  a < b (menor que)
+//  a = b (maior ou igual a)
+//  a <= b (menor ou igual a)
+//  a == b (igual a)
+//  a != b (diferente de)
+
+
+
+int main(){
+    int a = 10;
+    int b = 20;
+
+    printf("a > b: %d\n", a >= b);
+    printf("a < b: %d\n", a <= b);
+    printf("a == b: %d\n", a == b);
+    printf("a != b: %d\n", a != b);
+
+    return 0;
+
+
+
+
+
+
+
+
+
+}
